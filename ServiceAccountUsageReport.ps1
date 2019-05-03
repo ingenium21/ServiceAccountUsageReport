a2136ac3c9f75d26156f4432f7fb1341af862879
Renato Regalado <renato.regalado@gmail.com>
2019-05-03 06:11:05 -0500

    added author info

--- ServiceAccountUsageReport.ps1
+++ ServiceAccountUsageReport.ps1
@@ -1,9 +1,12 @@
     
 <#
+Author: Renato Regalado
+Date: 05/03/2019
+changelog: created script
+#>
+<#
 	.SYNOPSIS
-        To check if a particular account is being used somewhere
-    .AUTHOR
-        Renato Regalado    
+        To check if a particular account is being used somewhere  
 	.DESCRIPTION
         Takes a username or user list parameter and iterates through each computer in the domain to check if it is being used 
         anywhere as a service or scheduled task
